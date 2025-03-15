be4e9f62bb4185343d99459ae9de4f6fd2f8605a
Ryota <143061672+Ry0t7@users.noreply.github.com>
2025-03-15 15:09:02 +0900

    Update swiper.js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -6,7 +6,7 @@ const swiper = new Swiper(".swiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
-    slidesPerView: 3,
+    slidesPerView: 1.5,
 
     coverflowEffect: {
         rotate: 50,
